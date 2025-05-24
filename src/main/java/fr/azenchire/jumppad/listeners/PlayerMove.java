5c5117681ae56673247e1cb2423022c16448f33e
AzenChire <loanazenchire@gmail.com>
2025-05-24 11:28:17 +0200

    🔄 Improved launch detection & added new features
    - Switched from PlayerInteractEvent to PlayerMoveEvent for more accurate jump pad detection
    - Added sneak prevention system to avoid triggering jump while sneaking
    - Implemented configurable particle trail effect when launched

--- src/main/java/fr/azenchire/jumppad/listeners/PlayerMove.java
+++ src/main/java/fr/azenchire/jumppad/listeners/PlayerMove.java
@@ -1,36 +1,53 @@
 package fr.azenchire.jumppad.listeners;
 
 import fr.azenchire.jumppad.Main;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
+
+import java.lang.module.Configuration;
 
 public class PlayerMove implements Listener {
 
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
+    public void onPlayerMove(PlayerMoveEvent event) {
+        FileConfiguration config = Main.getInstance().getConfig();
+        Block from = event.getFrom().getBlock();
+        Block to = event.getTo().getBlock();
         Player player = event.getPlayer();
-        Block block = event.getClickedBlock();
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK && event.getAction() != Action.LEFT_CLICK_BLOCK) {
-            if (block != null) {
-                if (block.getType() == Material.valueOf(Main.getInstance().getConfig().getString("jump-block"))) {
-                    Vector direction = player.getLocation().getDirection();
-                    Vector launch = (new Vector(direction.getX(), Main.getInstance().getConfig().getDouble("launch-vertical"), direction.getZ())).normalize().multiply(Main.getInstance().getConfig().getDouble("launch-multiplier"));
-                    if (Main.getInstance().getConfig().getBoolean("enable-sound", true)) {
-                        player.playSound(player.getLocation(), "entity.firework_rocket.launch", 1.0f, 1.0f);
-                    }
-                    if (Main.getInstance().getConfig().getBoolean("enable-message", true)) {
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', Main.getInstance().getConfig().getString("message")));
-                    }
-                    player.setVelocity(launch);
+
+        if (from.equals(to)) return;
+
+        if (to.getType() == Material.valueOf(config.getString("launch-block"))) {
+            if (config.getBoolean("prevent-sneak") && player.isSneaking()){
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("sneak-message")));
+                return;
+            }
+            if (config.getBoolean("enable-sound", true)) {
+                player.playSound(player.getLocation(), Sound.valueOf(config.getString("sound-name")), 1.0f, 1.0f);
+            }
+            if (config.getBoolean("enable-message", true)) {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("launch-message")));
+            }
+            if (config.getBoolean("enable-particles", true)) {
+                try {
+                    Particle particle = Particle.valueOf(config.getString("particle-type").toUpperCase());
+                    player.getWorld().spawnParticle(particle, player.getLocation(), 20, 0.3, 0.3, 0.3, 0);
+                } catch (IllegalArgumentException e) {
+                    Bukkit.getLogger().warning("Invalid particle type in config.yml");
                 }
             }
+
+            Vector direction = player.getLocation().getDirection();
+            Vector launch = (new Vector(direction.getX(), config.getDouble("launch-vertical"), direction.getZ())).normalize().multiply(config.getDouble("launch-multiplier"));
+            player.setVelocity(launch);
         }
     }
 }
